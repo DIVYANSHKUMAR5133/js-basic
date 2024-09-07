a436e9366ec7ea5593e575f61b9ce5f681ab23c9
DIVYANSHKUMAR5133 <146151608+DIVYANSHKUMAR5133@users.noreply.github.com>
2024-09-07 09:21:19 +0000

    container config done

---
+++ js_basics/test2.js
@@ -0,0 +1,12 @@
+const accountId=45515
+let accountName="jahah@akhaf"
+var accountPassword="123456"
+accountCity="lalganj"
+
+// accountId = 2 //not allowed
+accountName = "jaajia@jaij"
+accountPassword = "135222"
+accountCity = "jaipur"
+console.log(accountId);
+
+console.table([accountId, accountName, accountPassword, accountCity]);
